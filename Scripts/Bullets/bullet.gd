c946df69f438094bb0fbb86a1c362baa1b21b288
BubuClinton <konopkajakub180@gmail.com>
2026-08-24 00:19:43 +0200

    Shotgun
    Added shotgun

---
+++ Scripts/Bullets/bullet.gd
@@ -0,0 +1,14 @@
+class_name Bullet
+extends Node2D
+
+@export var speed: int
+@export var lifespan: int
+
+func _ready() -> void:
+	rotation = randf_range(-1,1)
+
+func _process(delta: float) -> void:
+	position += Vector2.RIGHT.rotated(rotation) * speed
+	lifespan -= 1
+	if lifespan <= 0:
+		queue_free()
---
+++ Scripts/Weapons/shotgun.gd
@@ -0,0 +1,25 @@
+class_name Shotgun
+extends Node2D
+
+@onready var bullet_scene = preload("res://Bullets/node_2d.tscn")
+@export var marker2d: Marker2D
+@export var timer: Timer
+var can_shoot_bull = true
+
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+	if cos(rotation) < 0:
+		scale = Vector2(1, -1)
+	else:
+		scale = Vector2(1, 1)
+	if Input.is_action_just_pressed("Use") and can_shoot_bull:
+		for i in range(10):
+			var bullet = bullet_scene.instantiate()
+			bullet.global_position = marker2d.position
+			add_child(bullet)
+		timer.start()
+		can_shoot_bull = false
+
+
+func _on_timer_timeout() -> void:
+	can_shoot_bull = true
